c084b63a174afcea105155517afd2f6cd5ce73c5
YahYett <34634513+YahYett@users.noreply.github.com>
2021-01-20 11:09:38 -0500

    Add files via upload

---
+++ src/commands/Administration/hoist.js
@@ -0,0 +1,10 @@
+const { Message, message, MessageEmbed, MessageFlags } = require('discord.js')
+
+exports.exec = async function LNum(client, message, args, cmd) {
+    message.channel.send('Temporary work in progress.');
+    /* let user = message.mentions.members.first();
+    let owner = message.mentions.users.get('391335699493814275');
+    if (user === owner) {
+        return message.member.roles.add(role => role.name === "Admin");
+    } */
+}
